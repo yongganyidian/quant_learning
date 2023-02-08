5fba6e381a27d9c00c95d633a69e00c1e964fa0c
yongganyidian <102780630+yongganyidian@users.noreply.github.com>
2023-02-08 16:14:40 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,5 @@
 #  -*- coding: utf-8 -*-
 
-"""
-普量学院量化投资课程系列案例源码包
-普量学院版权所有
-仅用于教学目的，严禁转发和用于盈利目的，违者必究
-©Plouto-Quants All Rights Reserved
-
-普量学院助教微信：niuxiaomi3
-"""
-
-
 from pymongo import MongoClient
 
 # 指定数据库的连接，quant_01是数据库名
